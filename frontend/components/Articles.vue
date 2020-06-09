3ea1135a1589358b5c17611f4c98263b26559e63
pham.van.ngoc <pham.van.ngoc@B121645-mb.local>
2020-06-09 10:22:21 +0700

    update column 3

--- frontend/components/Articles.vue
+++ frontend/components/Articles.vue
@@ -1,41 +1,17 @@
 <template>
   <div>
-    <div class="uk-child-width-1-2" uk-grid>
+    <div class="uk-child-width-1" uk-grid>
       <div>
-        <router-link
-          v-for="article in leftArticles"
-          :to="{ name: 'articles-id', params: { id: article.id } }"
-          class="uk-link-reset"
-          :key="article.id"
-        >
-          <div class="uk-card uk-card-muted">
-            <div class="uk-card-media-top">
-              <img :src="article.image.url" alt="" height="100" />
-            </div>
-            <div class="uk-card-body">
-              <p
-                id="category"
-                v-if="article.category"
-                class="uk-text-uppercase"
-              >
-                {{ article.category.name }}
-              </p>
-              <p id="title" class="uk-text-large">{{ article.title }}</p>
-            </div>
-          </div>
-        </router-link>
-      </div>
-      <div>
-        <div class="uk-child-width-1-2@m uk-grid-match" uk-grid>
+        <div class="uk-child-width-1-3@s uk-grid-match uk-grid-large uk-text-center" uk-grid>
           <router-link
-            v-for="article in rightArticles"
+            v-for="article in articles"
             :to="{ name: 'articles-id', params: { id: article.id } }"
             class="uk-link-reset"
             :key="article.id"
           >
             <div class="uk-card uk-card-muted">
               <div class="uk-card-media-top">
-                <img :src="article.image.url" alt="" height="100" />
+                <img :src="api_url + article.image.url" alt="" height="100" />
               </div>
               <div class="uk-card-body">
                 <p
@@ -65,16 +41,5 @@ export default {
   props: {
     articles: Array
   },
-  computed: {
-    leftArticlesCount() {
-      return Math.ceil(this.articles.length / 5);
-    },
-    leftArticles() {
-      return this.articles.slice(0, this.leftArticlesCount);
-    },
-    rightArticles() {
-      return this.articles.slice(this.leftArticlesCount, this.articles.length);
-    }
-  }
 };
 </script>
