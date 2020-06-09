39e69f278a0a27a58fe9358b650684fde486cf89
pham.van.ngoc <pham.van.ngoc@B121645-mb.local>
2020-06-09 10:59:07 +0700

    update blog

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -5,13 +5,11 @@
         <div class="uk-navbar-left">
 
           <ul class="uk-navbar-nav">
-              <li><a href="#modal-full" uk-toggle><span uk-icon="icon: table"></span></a></li>
               <li>
-                <a href="/">Strapi Blog
+                <a href="/">Design Playbook
                 </a>
               </li>
           </ul>
-
         </div>
 
         <div class="uk-navbar-right">
@@ -23,27 +21,6 @@
           </ul>
         </div>
     </nav>
-
-    <div id="modal-full" class="uk-modal-full" uk-modal>
-        <div class="uk-modal-dialog">
-            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
-            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
-                <div class="uk-background-cover" style="background-image: url('https://images.unsplash.com/photo-1493612276216-ee3925520721?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3308&q=80 3308w');" uk-height-viewport></div>
-                <div class="uk-padding-large">
-                    <h1 style="font-family: Staatliches;">Strapi blog</h1>
-                    <div class="uk-width-1-2@s">
-                        <ul class="uk-nav-primary uk-nav-parent-icon" uk-nav>
-                          <li v-for="category in categories">
-                            <router-link class="uk-modal-close" :to="{ name: 'categories-id', params: { id: category.id }}" tag="a">{{ category.name }}
-                            </router-link>
-                          </li>
-                        </ul>
-                    </div>
-                    <p class="uk-text-light">Built with strapi</p>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <nuxt />
   </div>
@@ -66,4 +43,4 @@ export default {
   }
 }
 
-</script>  
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-
+    <div v-for="hero in heroes"
+        v-if="hero.image"
+        id="hero"
+        class=" uk-section uk-height-small uk-flex uk-flex-center uk-flex-middle uk-background-cover uk-light uk-padding"
+        :data-src="hero.image.url"
+        uk-img>
+      >
+      <h1>
+        {{hero.title}}
+      </h1>
+      <button>{{ hero.button }}</button>
+    </div>
     <div class="uk-section">
       <div class="uk-container uk-container-large">
         <Articles :articles="articles"></Articles>
-
       </div>
     </div>
 
@@ -13,12 +23,15 @@
 
 <script>
 import articlesQuery from '~/apollo/queries/article/articles'
+import heroQuery from '~/apollo/queries/hero/heroes'
 import Articles from '~/components/Articles'
 
 export default {
   data() {
     return {
       articles: [],
+      heroes: [],
+      api_url: process.env.strapiBaseUri
     }
   },
   components: {
@@ -28,6 +41,13 @@ export default {
     articles: {
       prefetch: true,
       query: articlesQuery,
+      variables () {
+        return { id: parseInt(this.$route.params.id) }
+      }
+    },
+    heroes: {
+      prefetch: true,
+      query: heroQuery,
       variables () {
         return { id: parseInt(this.$route.params.id) }
       }
