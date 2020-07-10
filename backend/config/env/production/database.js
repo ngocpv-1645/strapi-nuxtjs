0732b4a4330b670aa45645e380ff1871b44088c1
pham.van.ngoc <pham.van.ngoc@B121645-mb.local>
2020-07-10 15:46:49 +0700

    update code

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -1,19 +1,19 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        schema: "public",
-        ssl: { rejectUnauthorized: false }
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
-      options: {}
-    }
-  }
+      options: {
+        ssl: false,
+      },
+    },
+  },
 });
